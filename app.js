8a008b34ee8db3d870d0b369e60f618c49c6e415
ahmedhegazii22 <ahegazy.am@gmail.com>
2026-03-15 22:31:05 +0200

    api : new tour

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const fs = require("fs")
 const app = express();
+app.use(express.json())
 app.get('/', (req, res) => {
   res.status(200).send('Hello World');
 });
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours.json`));
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 //api to get all tours
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json(
@@ -17,7 +18,18 @@ app.get('/api/v1/tours', (req, res) => {
     }
   );
 });
-
+//api to add new tour
+app.post('/api/v1/tours', (req, res)=>{
+  const newTour = req.body;
+  tours.push(newTour);
+  fs.writeFileSync(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours));
+  res.status(201).json({
+    status: 'success',
+    data: {
+      tour: newTour
+    }
+  });
+})
 // Start the server
 const port = 3000;
 app.listen(port, () => {
